6a5d5e867e2ff27104761f48358034a1823a500b
lexsam-sm7 <lexussammymwangi@gmail.com>
2025-10-09 22:14:39 +0300

    Add files via upload

---
+++ for_loops.c
@@ -0,0 +1,20 @@
+/*
+Name:Sammy Mwangi 
+Reg no:CT101/G/26462/25
+Description :use_of_loop
+*/
+#include<stdio.h>
+
+int main()
+{
+    //(start;step;stop)()
+    //for(intilisation;condition;stop)
+    int i;
+    
+    for(i=100;i>=50;i--){
+    printf("%d\n",i);
+    
+    }
+    
+    return 0;
+}
